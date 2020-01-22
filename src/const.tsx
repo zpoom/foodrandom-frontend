70da3bbdf5defed9217859e1af3624ba21980493
zpoom <poom007.sk135@gmail.com>
2020-01-22 17:12:05 +0700

    secrets google map api key

--- src/const.tsx
+++ src/const.tsx
@@ -1,4 +1,4 @@
-export const GOOGLE_MAP_API_KEY: string =
-  "AIzaSyBt5q87YgYxnD7UdqBF0kRT-05GkuHt5cU";
+export const GOOGLE_MAP_API_KEY: string = process.env.GOOGLE_MAP_API_KEY || "";
+// "AIzaSyBt5q87YgYxnD7UdqBF0kRT-05GkuHt5cU";
 export const baseUrl: string =
   "https://us-central1-food-random-265418.cloudfunctions.net/api";
