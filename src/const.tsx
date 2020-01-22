f448851af314350438092ea4622efbda7954b684
zpoom <poom007.sk135@gmail.com>
2020-01-22 18:24:54 +0700

    enable maps

--- src/const.tsx
+++ src/const.tsx
@@ -1,3 +1,4 @@
-export const GOOGLE_MAP_API_KEY: string = process.env.GOOGLE_MAP_API_KEY || "";
+export const GOOGLE_MAP_API_KEY: string =
+  "AIzaSyBt5q87YgYxnD7UdqBF0kRT-05GkuHt5cU";
 export const baseUrl: string =
   "https://us-central1-food-random-265418.cloudfunctions.net/api";
